0edc6cdf5f57cede170d643ef9e5c9e6d28acf34
jonnyedwards888 <edwardsjonny547@gmail.com>
2026-03-27 18:13:55 +0000

    fix RLS policy issue

--- src/lib/dataAccess.ts
+++ src/lib/dataAccess.ts
@@ -44,6 +44,8 @@ export async function createNote(
     emoji: note.emoji,
     drawing_data: note.drawing_data,
     is_starred: note.is_starred,
+    created_at: note.created_at,
+    updated_at: note.updated_at,
   });
 
   return note;
